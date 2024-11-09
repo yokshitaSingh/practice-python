12ab08b86d8662f793135ec70ef9bd9eab3797fa
yokshitaSingh <yokshita1901@gmail.com>
2024-11-09 09:23:39 -0800

    Adding Positive .py

---
+++ positive.py
@@ -0,0 +1,9 @@
+def check(n):
+    if n>0:
+        print("positive")
+    elif n<0:
+       print("negative")
+    else:
+       print("no.is equals to zero")
+n=int(input("enter a no."))
+check(n)
