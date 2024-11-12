0a3628e4a8873450980597b8b6c5b5c65025175e
yokshitaSingh <yokshita1901@gmail.com>
2024-11-12 10:01:57 -0800

    Delete positive.py

--- positive.py
+++
@@ -1,9 +0,0 @@
-def check(n):
-    if n>0:
-        print("positive")
-    elif n<0:
-       print("negative")
-    else:
-       print("no.is equals to zero")
-n=int(input("enter a no."))
-check(n)
